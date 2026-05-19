44617b8d8cda776f4ded18f8ea0a85747d33aac3
zohidbek <mengliboyevzohidbek@gmail.com>
2026-05-19 19:19:24 +0500

    authentication - registration and login with jwt

--- main.py
+++ main.py
@@ -101,6 +101,24 @@ async def user_posts_page(
         "user_posts.html",
         {"posts": posts, "user": user, "title": f"{user.username}'s Posts"},
     )
+    
+
+@app.get("/login", include_in_schema=False)
+async def login_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "login.html",
+        {"title": "Login"},
+    )
+
+
+@app.get("/register", include_in_schema=False)
+async def register_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "register.html",
+        {"title": "Register"},
+    )
 
 
 @app.exception_handler(StarletteHTTPException)
